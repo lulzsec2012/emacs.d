5c70d35b9409cbc496af24099f0ba63fa8c5c487
lizhi.lu <lizhi.lu@bitmaincorp.com>
2020-09-02 06:29:52 +0000

    back-and-forth bind to C-f6

--- init.el
+++ init.el
@@ -22,32 +22,3 @@
   (org-babel-load-file "~/.emacs.d/configuration.org")
   )
 (load-file "~/.emacs.d/Emacs_ingenic/.emacs")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(comint-completion-autolist nil)
- '(comint-completion-fignore (quote ("~" "#" "%" ".o")))
- '(comint-input-ignoredups (quote t))
- '(comint-input-ring-size 500)
- '(font-lock-maximum-size
-   (quote
-    ((c-mode . 256000)
-     (c++-mode . 256000)
-     (verilog-mode . 1024000))))
- '(history-length 500)
- '(line-number-display-limit 500000)
- '(next-line-add-newlines nil)
- '(package-selected-packages
-   (quote
-    (ace-window goto-chg avy expand-region wrap-region ivy-rich smex flx counsel yasnippet-snippets which-key helpful company-auctex auctex company-math ob-async org-super-agenda org-pomodoro workgroups2 web-mode virtualenvwrapper use-package undo-tree typescript-mode symon symbol-overlay solarized-theme smartparens smart-jump rspec-mode realgud rainbow-mode rainbow-delimiters protobuf-mode projectile powerline paredit org-plus-contrib org-bullets nord-theme multi-term magit-lfs ivy-xref imenu-list hydra highlight-indent-guides git-timemachine ggtags geiser flycheck-package ein diff-hl deadgrep company-statistics cmake-font-lock bazel-mode bash-completion auto-package-update auto-compile)))
- '(scroll-bar-mode (quote right))
- '(shell-completion-fignore (quote ("~" "#" "%" ".o"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
