7a3eb65371b407bac9ce5cf29e0634b746b2dffa
lulizhi <lulizhi001@gmail.com>
2026-06-15 16:55:34 +0800

    Reorganize Emacs config: clean init.el, tune performance, centralize opencode
    - init.el: strip auto-generated Customize blocks, simplify comments
    
    - configuration.org: centralize opencode install from :vc to setq, fix magit up/down arrow
    
    - configuration.org: add LRU eglot process eviction, switch jinx/corfu to manual trigger
    
    - configuration.org: tune gc-cons-threshold and read-process-output-max
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- init.el
+++ init.el
@@ -1,69 +1,37 @@
 ;;; init.el --- -*- lexical-binding: t; -*-
 
-;; Compatibility shims for Emacs 30+ functions missing from this build.
-;; Needed by the latest magit.
+;; Compatibility shims for Emacs 30
 (unless (fboundp 'set-local)
   (defun set-local (variable value)
     "Set VARIABLE to VALUE in the current buffer."
     (set (make-local-variable variable) value)))
 (unless (fboundp 'any)
   (defun any (pred list)
-    "Return non-nil if PRED returns non-nil for any element of LIST.
-Like `cl-some' with the predicate as the first argument."
+    "Return non-nil if PRED returns non-nil for any element of LIST."
     (cl-some pred list)))
 
 (require 'package)
 (setq package-archives
-      '(("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-        ("melpa-stable" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/stable-melpa/")
-        ("gnu"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-        ;; ("gnu-devel"   . "https://elpa.gnu.org/devel/")
-        ("nongnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
-        ))
+      '(("melpa"  . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+        ("gnu"    . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("nongnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")))
 (setq package-install-upgrade-built-in t)
 (package-initialize)
+
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (setq-default buffer-file-coding-system 'utf-8)
-(setq native-comp-async-report-warnings-errors nil) ;; suppress the native compile warning.
-;; Ensure that use-package is installed.
-;;
-;; If use-package isn't already installed, it's extremely likely that this is a
-;; fresh installation! So we'll want to update the package repository and
-;; install use-package before loading the literate configuration.
-(when (not (package-installed-p 'use-package))
+(setq native-comp-async-report-warnings-errors nil)
+
+;; Bootstrap use-package for literate config
+(unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
 (require 'org)
 (require 'bind-key)
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 (load-file "~/.emacs.d/emacs-extras/key-bindings.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(comint-completion-autolist nil)
- '(comint-completion-fignore '("~" "#" "%" ".o"))
- '(comint-input-ignoredups 't)
- '(comint-input-ring-size 500)
- '(font-lock-maximum-size
-   '((c-mode . 256000) (c++-mode . 256000) (verilog-mode . 1024000)))
- '(history-length 500)
- '(line-number-display-limit 500000)
- '(next-line-add-newlines nil)
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((opencode :url "https://codeberg.org/sczi/opencode.el.git")))
- '(scroll-bar-mode 'right)
- '(shell-completion-fignore '("~" "#" "%" ".o")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-comment-delimiter-face ((t (:inherit italic :foreground "#969faf"))))
- '(font-lock-comment-face ((t (:inherit italic :foreground "#969faf")))))
